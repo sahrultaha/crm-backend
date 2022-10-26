685df0ccdf00f21afad252b0723f79218754c59b
arnold yonata <arnold.yonata@gmail.com>
2022-10-26 10:27:32 +0800

    working login page

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -5,10 +5,11 @@ namespace Tests\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class LoginTest extends DuskTestCase
 {
-    use DatabaseMigrations;
+    use RefreshDatabase;
 
     public $seed = true;
 
@@ -23,9 +24,10 @@ class LoginTest extends DuskTestCase
             $browser->visit(env('FRONTEND_URL').'/login')
                 ->waitForText('Email')
                 ->waitForText('Remember me')
-                ->type('#email', env('ADMIN_EMAIL'))
-                ->type('#password', env('ADMIN_PASSWORD'))
+                ->type('#email', 'admin@example.com')
+                ->type('#password', 'password')
                 ->press('LOGIN')
+                ->waitForText('Dashboard')
                 ->assertPathIs('/dashboard');
         });
     }
