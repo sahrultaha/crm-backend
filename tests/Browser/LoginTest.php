f712c6cf879a5bafd2ee0bf3944aa04d9a8cb6e4
arnold yonata <arnold.yonata@gmail.com>
2022-10-26 09:52:52 +0800

    Login test

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    public $seed = true;
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(env('FRONTEND_URL').'/login')
+                ->waitForText('Email')
+                ->waitForText('Remember me')
+                ->type('#email', env('ADMIN_EMAIL'))
+                ->type('#password', env('ADMIN_PASSWORD'))
+                ->press('LOGIN')
+                ->assertPathIs('/dashboard');
+        });
+    }
+}
