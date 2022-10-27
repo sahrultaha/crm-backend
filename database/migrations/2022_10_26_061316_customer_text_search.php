75bdd8e26dc77fdf10de55c65ae727b147f0e903
arnold yonata <arnold.yonata@gmail.com>
2022-10-27 11:20:03 +0800

    fulltext trigger and ic_number

--- database/migrations/2022_10_26_061316_customer_text_search.php
+++ database/migrations/2022_10_26_061316_customer_text_search.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
@@ -13,15 +14,27 @@ return new class extends Migration
      */
     public function up()
     {
-        // @TODO support more database, sqlite, mysql
         if (env('DB_CONNECTION') === 'pgsql') {
             DB::statement("ALTER TABLE customer
             ADD COLUMN fulltext tsvector
             GENERATED ALWAYS AS (to_tsvector('english', 
-            coalesce(name, '') || ' ' || coalesce(email, '') || ' ' || coalesce(mobile_number, '')  
+            coalesce(name, '') || ' ' || coalesce(email, '') || ' ' || coalesce(mobile_number, '') || ' ' || coalesce(ic_number, '')
             )) STORED");
             DB::statement('CREATE INDEX IF NOT EXISTS fulltext_idx ON customer USING GIN (fulltext)');
-        } 
+
+            return;
+        }
+        Schema::table('customer', function (Blueprint $table) {
+            $table->addColumn('text', 'fulltext', ['nullable' => true]);
+        });
+        DB::statement("CREATE TRIGGER IF NOT EXISTS customer_insert_trigger AFTER INSERT ON customer
+BEGIN
+	update customer set fulltext = NEW.name || ' ' || NEW.email || ' ' || NEW.mobile_number WHERE id = NEW.id;
+END;");
+        DB::statement("CREATE TRIGGER IF NOT EXISTS customer_update_trigger UPDATE ON customer
+BEGIN
+	update customer set fulltext = NEW.name || ' ' || NEW.email || ' ' || NEW.mobile_number WHERE id = OLD.id;
+END;");
     }
 
     /**
@@ -31,10 +44,12 @@ return new class extends Migration
      */
     public function down()
     {
-        // @TODO support more database, sqlite, mysql
-        if (env('DB_CONNECTION') === 'pgsql') {
-            DB::statement('CREATE INDEX fulltext_idx ON customer USING GIN (fulltext)');
-            Schema::dropColumns('customer', 'fulltext');
+        if (env('DB_CONNECTION') === 'sqlite') {
+            DB::statement('DROP TRIGGER IF EXISTS customer_update_trigger');
+            DB::statement('DROP TRIGGER IF EXISTS customer_insert_trigger');
         }
+        Schema::table('customer', function (Blueprint $table) {
+            $table->dropColumn('fulltext');
+        });
     }
 };
