337cfda0e06dc097e9e333166b293bd05e901212
Alif <alif.rafiuddin@dst.com.bn>
2022-11-03 11:42:30 +0800

    Use temp dir and unlink file when done

--- tests/Browser/ViewCustomersTest.php
+++ tests/Browser/ViewCustomersTest.php
@@ -10,16 +10,9 @@ use Tests\CustomDuskTestCase;
 
 class ViewCustomersTest extends CustomDuskTestCase
 {
-    // use DatabaseMigrations;
+    use DatabaseMigrations;
 
-    // public $seed = true;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->artisan('migrate:fresh');
-        $this->artisan('db:seed');
-    }
+    public $seed = true;
 
     public function test_can_view_customers()
     {
@@ -27,7 +20,7 @@ class ViewCustomersTest extends CustomDuskTestCase
             $this->loginAsAdmin($browser);
             $this->createNewCustomer($browser);
 
-            sleep(1);
+            $browser->pause(1000);
 
             $elements = $browser->driver->findElements(WebDriverBy::tagName('img'));
             $this->assertCount(2, $elements);
@@ -37,18 +30,20 @@ class ViewCustomersTest extends CustomDuskTestCase
             $sample_file = base_path('tests/Browser/photos/600x300.png');
             $sample_file_size = filesize($sample_file);
             $sample_file_hash = hash_file('sha1', $sample_file);
-            $temp_dir = base_path('tests/Browser/temp/');
+            $temp_dir = sys_get_temp_dir();
 
             for ($i = 0; $i < count($img_elements) - 1; $i++) {
                 $src = $img_elements[$i]->getAttribute('src');
                 $response = Http::get($src);
 
-                $test_file = "$temp_dir${i}.png";
+                $test_file = "$temp_dir/$i.png";
                 file_put_contents($test_file, $response->body());
                 $this->assertEquals($sample_file_size, filesize($test_file));
 
                 $test_file_hash = hash_file('sha1', $test_file);
                 $this->assertEquals($sample_file_hash, $test_file_hash);
+
+                unlink($test_file);
             }
         });
     }
