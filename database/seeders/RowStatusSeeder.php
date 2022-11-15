c17e3088e46a7a7d497af3392d867934ce60f61c
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-15 09:02:14 +0800

    add row status

---
+++ database/seeders/RowStatusSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RowStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('row_status')->insert([
+            [
+                'id' => 1,
+                'name' => 'New',
+            ],
+            [
+                'id' => 2,
+                'name' => 'Success',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Fail',
+            ],
+        ]);
+    }
+}
