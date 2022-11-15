4f10a1bf1d4ec5479d81c776087535a6ea68e858
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-15 09:29:39 +0800

    file bulk imsi

---
+++ database/migrations/2022_11_15_100000_create_file_bulk_imsi.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_bulk_imsi', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('imsi');
+
+            $table->smallInteger('pin');
+            $table->integer('puk_1');
+            $table->integer('puk_2');
+            $table->string('ki')->nullable();
+            $table->bigInteger('imsi_id')->nullable()->index();
+            $table->bigInteger('file_id')->index();
+            $table->smallInteger('imsi_type_id')->index();
+            $table->smallInteger('row_status_id')->index();
+
+            $table->foreign('imsi_type_id')->references('id')->on('imsi_type');
+            $table->foreign('imsi_id')->references('id')->on('imsi');
+            $table->foreign('file_id')->references('id')->on('file');
+            $table->foreign('row_status_id')->references('id')->on('row_status');
+            $table->timestampsTz(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_bulk_imsi');
+    }
+};
