85d333a3a239e1ec75192a22ccd59362088c6a5f
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-15 11:25:21 +0800

    biginteger imsi

--- database/migrations/2022_11_15_100000_create_file_bulk_imsi.php
+++ database/migrations/2022_11_15_100000_create_file_bulk_imsi.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('file_bulk_imsi', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('imsi');
+            $table->bigInteger('imsi');
 
             $table->smallInteger('pin');
             $table->integer('puk_1');
