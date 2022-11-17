6b84114a3dba560017fd465aa05a9f4aa36682c9
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-17 17:04:15 +0800

    file_bulk_imsi_count* attributes

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -13,6 +14,14 @@ class File extends Model
 
     protected $table = 'file';
 
+    protected $appends = [
+        'file_bulk_imsi_count_new',
+        'file_bulk_imsi_count_success',
+        'file_bulk_imsi_count_fail',
+    ];
+
+    protected $bulk_imsi_count = null;
+
     public function fileCategory(): BelongsTo
     {
         return $this->belongsTo(FileCategory::class);
@@ -21,5 +30,50 @@ class File extends Model
     public function fileRelationTypes(): BelongsToMany
     {
         return $this->belongsToMany(FileRelationType::class);
+    }
+
+    protected function fileBulkImsiCountNew(): Attribute
+    {
+        return $this->getFileBulkImsiCountByKey(RowStatus::NEW);
+    }
+
+    protected function fileBulkImsiCountSuccess(): Attribute
+    {
+        return $this->getFileBulkImsiCountByKey(RowStatus::SUCCESS);
+    }
+
+    protected function fileBulkImsiCountFail(): Attribute
+    {
+        return $this->getFileBulkImsiCountByKey(RowStatus::FAIL);
+    }
+
+    protected function getFileBulkImsiCountByKey($key)
+    {
+        $counters = $this->getFileBulkImsiCount();
+        $count = 0;
+        foreach ($counters as $counter) {
+            if ($counter['row_status_id'] === $key) {
+                $count = $counter['count_by_row_status_id'];
+            }
+        }
+
+        return new Attribute(
+            get: fn () => $count,
+        );
+    }
+
+    protected function getFileBulkImsiCount(): array
+    {
+        if (! $this->exists || $this->file_category_id !== FileCategory::BULK_IMSI_FILE) {
+            return $this->bulk_imsi_count = [];
+        }
+
+        if ($this->bulk_imsi_count !== null) {
+            return $this->bulk_imsi_count;
+        }
+
+        $repo = new \App\Repositories\FileBulkImsiRepository();
+
+        return $this->bulk_imsi_count = $repo->selectByFileIdGroupByRowStatusID($this->id)->toArray();
     }
 }
