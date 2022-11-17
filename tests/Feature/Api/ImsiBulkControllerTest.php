73fd806f851378fd277ea09cd305642f5118eb72
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-17 11:55:17 +0800

    guest test access

--- tests/Feature/Api/ImsiBulkControllerTest.php
+++ tests/Feature/Api/ImsiBulkControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Api;
 
 use App\Models\User;
-use Database\Seeders\Skeleton;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
@@ -12,9 +11,15 @@ class ImsiBulkControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function test_index_guest()
+    {
+        $response = $this->getJson('/api/imsi/bulk');
+
+        $response->assertUnauthorized();
+    }
+
     public function test_index_access()
     {
-        $this->seed(Skeleton::class);
         Sanctum::actingAs(User::factory()->create());
         $response = $this->getJson('/api/imsi/bulk');
 
