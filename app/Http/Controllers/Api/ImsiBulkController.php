0b58522bc2fc27b9dbdab5595aa0dd56f78f525d
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-17 14:07:01 +0800

    use BaseRepository::paginate to serve index

--- app/Http/Controllers/Api/ImsiBulkController.php
+++ app/Http/Controllers/Api/ImsiBulkController.php
@@ -3,19 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Repositories\FileBulkImsiRepository;
+use App\Models\File;
+use App\Repositories\BaseRepository;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class ImsiBulkController extends Controller
 {
-    public function __construct(FileBulkImsiRepository $repository)
-    {
-        $this->repository = $repository;
-    }
-
     public function index(Request $request): AnonymousResourceCollection
     {
-        return \Illuminate\Http\Resources\Json\JsonResource::collection($this->repository->paginate());
+        $repo = new BaseRepository(new File());
+        $limit = $request->get('limit', 10);
+        $sort = $request->get('sort', 'desc');
+        $page = $request->get('page', 1);
+        $attributes = [['file_category_id' => \App\Models\FileCategory::BULK_IMSI_FILE]];
+
+        return JsonResource::collection($repo->paginate($limit, $sort, $page, $attributes));
     }
 }
--- tests/Feature/Api/ImsiBulkControllerTest.php
+++ tests/Feature/Api/ImsiBulkControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Api;
 
+use App\Models\File;
 use App\Models\User;
+use App\Repositories\BaseRepository;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
@@ -20,9 +22,15 @@ class ImsiBulkControllerTest extends TestCase
 
     public function test_index_access()
     {
+        $this->seed(\Database\Seeders\FileSeeder::class);
+        $repo = new BaseRepository(new File());
+        $paginator = $repo->paginate();
+        $this->assertEquals(15, $paginator->total());
         Sanctum::actingAs(User::factory()->create());
         $response = $this->getJson('/api/imsi/bulk');
 
         $response->assertOk();
+
+        $response->assertJsonPath('meta.total', 5);
     }
 }
