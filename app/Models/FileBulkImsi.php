17f81f71296ea87e35af34b1d5ec10275431dde9
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-17 14:51:29 +0800

    file bulk imsi factory

--- app/Models/FileBulkImsi.php
+++ app/Models/FileBulkImsi.php
@@ -2,19 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FileBulkImsi extends Model
 {
+    use HasFactory;
+
     protected $table = 'file_bulk_imsi';
 
     protected $fillable = [
         'imsi',
-        'imsi_status_id',
-        'imsi_type_id',
         'pin',
         'puk_1',
         'puk_2',
+        'imsi_type_id',
         'row_status_id',
         'file_id',
         'imsi_id',
