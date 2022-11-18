eee3d916d7b9b5ce508c76b5b94e9961362ba35f
Sahrul Taha <Sahrul.taha@dst.com.bn>
2022-11-18 15:51:34 +0800

    Update Customer Details: Added: - Endpoints for files, customer update -Browser & features test
    Changes:
    - IcCheck mode - update and create
    -Customer repository - add customer/update customer

--- tests/Browser/CheckCustomerIcTest.php
+++ tests/Browser/CheckCustomerIcTest.php
@@ -11,14 +11,16 @@ class CheckCustomerIcTest extends CustomDuskTestCase
     public function test_users_can_check_ic()
     {
         $this->assertDatabaseCount('customer', 30);
+
         $customer = Customer::find(1);
-        $this->browse(function (Browser $browser) use ($customer) {
+        $this->browse(function (Browser $browser) {
             $this->loginAsAdmin($browser);
+            $this->createNewCustomer($browser, '77661234');
             $browser
             ->visit(env('FRONTEND_URL').'/customers/create')
             ->waitForText('CREATE')
             ->typeSlowly('#name', 'Lorem')
-            ->typeSlowly('#icNumber', $customer->ic_number)
+            ->typeSlowly('#icNumber', '77661234')
             ->select('#icTypeId', '1')
             ->pause(5000)
             ->waitForText('Customer already exist!');
