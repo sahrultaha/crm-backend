f7a9808e866547a6c5b8d4193be88d3c48515108
Arnold Yonata <arnold.yonata@gmail.com>
2022-11-22 19:39:48 +0800

    update pack seeder

--- database/seeders/PackSeeder.php
+++ database/seeders/PackSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Pack;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class PackSeeder extends Seeder
 {
@@ -14,31 +14,7 @@ class PackSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('pack')->insert([
-            [
-                'number_id' => 1,
-                'imsi_id' => 1,
-                'product_id' => 1,
-                'pack_type_id' => 1,
-                'installation_date' => now()->subYear(),
-                'expiry_date' => now()->addYears(10),
-            ],
-            [
-                'number_id' => 2,
-                'imsi_id' => 2,
-                'product_id' => 2,
-                'pack_type_id' => 2,
-                'installation_date' => now()->subYear(),
-                'expiry_date' => now()->addYears(10),
-            ],
-            [
-                'number_id' => 3,
-                'imsi_id' => 3,
-                'product_id' => 3,
-                'pack_type_id' => 3,
-                'installation_date' => now()->subYear(),
-                'expiry_date' => now()->addYears(10),
-            ],
-        ]);
+        Pack::factory()->count(10)
+                ->create();
     }
 }
